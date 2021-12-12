3b091753932c27a2fc49a71c0d5cef1a3031f749
jovnascimento <67511057+jovnascimento@users.noreply.github.com>
2021-12-12 16:36:52 -0300

    Atualização BD

--- site-vinhos/frontend/frontend/src/app/app-routing.module.ts
+++ site-vinhos/frontend/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { NewUserComponent } from './auth/new-user/new-user.component';
 
-const routes: Routes = [{ path: 'vinhos', loadChildren: () => import('./vinhos/vinhos.module').then(m => m.VinhosModule) }];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: NewUserComponent},
+  { path: 'logout', component: LogoutComponent},
+  { path: 'vinhos', loadChildren: () => import('./vinhos/vinhos.module').then(m => m.VinhosModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
